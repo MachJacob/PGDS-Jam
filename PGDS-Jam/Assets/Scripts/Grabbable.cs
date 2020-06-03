72a728d93256f7e9949c0f9f93b693d5c6318f11
Jacob Carr <jcarr343@hotmail.co.uk>
2020-06-03 15:42:05 +0100

    Added ability to drag multiple objects

---
+++ PGDS-Jam/Assets/Scripts/Grabbable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour
+{
+    public float teleForce;
+    private Rigidbody2D rb;
+    private Camera cam;
+    private LineRenderer lr;
+    private bool drag;
+    private Vector2 point;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        drag = false;
+
+        cam = Camera.main;
+
+        lr = gameObject.AddComponent<LineRenderer>();
+        lr = GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+        lr.startColor = new Color(205 / 255f, 90 / 255f, 214 / 255f, 150 / 255f);
+        lr.endColor = new Color(205 / 255f, 90 / 255f, 214 / 255f, 150 / 255f);
+        lr.startWidth = 0.025f;
+        lr.endWidth = 0.025f;
+    }
+
+    public void Drag(Vector3 _point, float _force)
+    {
+        lr.SetPosition(0, transform.TransformPoint(point));
+        lr.SetPosition(1, _point);
+
+        Vector2 dir = _point - transform.position;
+        float dot = Vector2.Dot(rb.velocity, dir);
+        float backforce = 1;
+        if (dot < 0)
+        {
+            backforce = 4;
+            //dir.Normalize();
+        }
+        rb.AddForceAtPosition(dir * backforce * _force * Time.deltaTime, transform.TransformPoint(point));
+        //rb.AddForce(dir * backforce * teleForce * Time.deltaTime);
+        //Debug.Log(dir);
+    }
+
+    public void SetDrag(bool _state)
+    {
+        drag = _state;
+        lr.enabled = _state;
+        if (_state)
+        {
+            rb.gravityScale = 0f;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+
+        point = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    }
+}
--- PGDS-Jam/Assets/Scripts/Telekinesis.cs
+++ PGDS-Jam/Assets/Scripts/Telekinesis.cs
@@ -4,67 +4,48 @@ using UnityEngine;
 
 public class Telekinesis : MonoBehaviour
 {
-    public float teleForce;
-    private Rigidbody2D rb;
-    private Camera cam;
-    private LineRenderer lr;
-    private bool drag;
-    private Vector2 point;
+    private Collider2D col;
+    private List<Grabbable> dragged;
+    public float TeleForce;
 
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        drag = false;
-
-        cam = Camera.main;
-
-        lr = gameObject.AddComponent<LineRenderer>();
-        lr = GetComponent<LineRenderer>();
-        lr.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
-        lr.startColor = new Color(205/255f, 90 / 255f, 214 / 255f, 150 / 255f);
-        lr.endColor = new Color(205 / 255f, 90 / 255f, 214 / 255f, 150 / 255f);
-        lr.startWidth = 0.025f;
-        lr.endWidth = 0.025f;
+        col = GetComponent<Collider2D>();
+        dragged = new List<Grabbable>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos2d = new Vector2(mousePos.x, mousePos.y);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        transform.position = mousePos2D;
         if (Input.GetMouseButtonDown(0))
         {
-            //if (Physics2D.Raycast(mousePos2d, Vector2.zero))
-            if (Physics2D.OverlapPoint(mousePos2d) == GetComponent<Collider2D>())
+            ContactFilter2D filter = new ContactFilter2D();
+            List<Collider2D> list = new List<Collider2D>();
+            if (Physics2D.OverlapCollider(col, filter, list) > 0)
             {
-                drag = true;
-                rb.gravityScale = 0;
-                point = mousePos - transform.position;
-                lr.enabled = true;
+                foreach(Collider2D f in list)
+                {
+                    if (f.gameObject.GetComponent<Grabbable>())
+                    {
+                        dragged.Add(f.GetComponent<Grabbable>());
+                        f.GetComponent<Grabbable>().SetDrag(true);
+                    }
+                }
             }
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            drag = false;
-            rb.gravityScale = 1f;
-            lr.enabled = false;
-        }
-
-        if (drag)
-        {
-            lr.SetPosition(0, transform.TransformPoint(point));
-            lr.SetPosition(1, mousePos2d);
-
-            Vector2 dir = mousePos - transform.position;
-            float dot = Vector2.Dot(rb.velocity, dir);
-            float backforce = 1;
-            if (dot < 0)
+            foreach(Grabbable grab in dragged)
             {
-                backforce = 4;
-                //dir.Normalize();
+                grab.SetDrag(false);
             }
-            rb.AddForceAtPosition(dir * backforce * teleForce * Time.deltaTime, transform.TransformPoint(point));
-            //rb.AddForce(dir * backforce * teleForce * Time.deltaTime);
-            //Debug.Log(dir);
+            dragged.Clear();
+        }
+        foreach(Grabbable grab in dragged)
+        {
+            grab.Drag(transform.position, TeleForce);
         }
     }
 }
